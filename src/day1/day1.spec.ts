75c5ede5fa8d84fcd2fa4d35f671234b59e09043
Loris Bognanni <loris@codemade.co.uk>
2021-12-01 18:34:52 +0000

    :gift: Day 1

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,7 +1,29 @@
 import day1 from './index';
 
 describe('On Day 1', () =>{
-    it(`part1 is identity function`, ()=>{
-        expect(day1.solveForPartOne('hello')).toBe('hello');
+    it(`Should solve part 1 for the example case`, ()=>{
+        expect(day1.solveForPartOne(`199
+200
+208
+210
+200
+207
+240
+269
+260
+263`)).toBe("7");
     })
+
+    it(`Should solve part 2 for the example case`, ()=>{
+        expect(day1.solveForPartTwo(`199
+200
+208
+210
+200
+207
+240
+269
+260
+263`)).toBe("5");
+    });
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -6,12 +6,38 @@ class Day1 extends Day {
         super(1);
     }
 
+    countIncreases(lines: number[]){
+        let count = 0;
+        for(let i = 1; i< lines.length; ++i)
+        {
+            if(lines[i] > lines[i-1])
+            {
+                count++;
+            }
+        }
+
+        return count.toString();
+    }
+
     solveForPartOne(input: string): string {
-        return input;
+        const lines = input.split('\n').map(x => parseInt(x, 10));
+        return this.countIncreases(lines);
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+
+        const lines = input.split('\n').map(x => parseInt(x, 10));
+        let groups: number[] = [];
+
+        lines.forEach((line, i) =>{
+            if(i > 1)
+                groups [i - 1] += line;
+            if(i > 2)
+                groups[i-2] += line;
+            groups[i] = line;
+        });
+        
+        return this.countIncreases(groups);
     }
 }
 
