b046c7961457cd8d5ea96467ddc0df9c4aef4b1c
Loris Bognanni <loris@codemade.co.uk>
2021-12-07 19:13:43 +0000

    :gift: Day 7 part 2

--- src/day7/day7.spec.ts
+++ src/day7/day7.spec.ts
@@ -6,4 +6,14 @@ describe("On Day 7", () => {
   it(`part 1 can solve for example`, () => {
     expect(day7.solveForPartOne(example)).toBe("37");
   });
+
+  it(`can calculate the right fuel distance`, () => {
+    expect(day7.fuelDistance(4)).toBe(10);
+    expect(day7.fuelDistance(5)).toBe(15);
+    expect(day7.fuelDistance(11)).toBe(66);
+    expect(day7.fuelDistance(9)).toBe(45);
+  });
+  it(`part 2 can solve for example`, () => {
+    expect(day7.solveForPartTwo(example)).toBe("168");
+  });
 });
--- src/day7/index.ts
+++ src/day7/index.ts
@@ -5,24 +5,32 @@ class Day7 extends Day {
     super(7);
   }
 
-  solveForPartOne(input: string): string {
-    const values = input
+  getValues(input: string): number[] {
+    return input
       .split(/,/g)
       .filter(Boolean)
       .map((x) => parseInt(x, 10));
+  }
 
-    const positions = Object.keys(
+  getDistinctValues(values: number[]): number[] {
+    return Object.keys(
       values.reduce((acc, cur) => {
         acc[cur] = 0;
         return acc;
       }, {} as { [key: number]: number })
     ).map((x) => parseInt(x, 10));
-    
+  }
+
+  solve(input: string, method: (dist: number) => number): string {
+    const values = this.getValues(input);
+    const maxValue = values.reduce((prev, cur) => Math.max(prev, cur));
+    const minValue = values.reduce((prev, cur) => Math.min(prev, cur));
+
     let minMovements = Number.MAX_VALUE;
 
-    for (const pos of positions) {
+    for (let pos = minValue; pos <= maxValue; ++pos) {
       const movements = values.reduce(
-        (acc, cur) => acc + Math.abs(pos - cur),
+        (acc, cur) => acc + method(Math.abs(pos - cur)),
         0
       );
       if (movements < minMovements) minMovements = movements;
@@ -31,8 +39,28 @@ class Day7 extends Day {
     return minMovements.toString();
   }
 
+  solveForPartOne(input: string): string {
+    return this.solve(input, (x) => x);
+  }
+
+  lookup: { [key: number]: number } = {};
+
+  fuelDistance(val: number): number {
+    if (val <= 1) {
+      return val;
+    }
+
+    if (this.lookup[val]) {
+      return this.lookup[val];
+    }
+
+    const result = val + this.fuelDistance(val - 1);
+    this.lookup[val] = result;
+    return result;
+  }
+
   solveForPartTwo(input: string): string {
-    return "input";
+    return this.solve(input, (x) => this.fuelDistance(x));
   }
 }
 
