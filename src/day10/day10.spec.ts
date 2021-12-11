0d0ca33c6d24ae796b9248af2cb8cb29f9e2255e
Loris Bognanni <loris@codemade.co.uk>
2021-12-11 18:04:26 +0000

    :gift: day 10 part 2

--- src/day10/day10.spec.ts
+++ src/day10/day10.spec.ts
@@ -15,4 +15,11 @@ describe("On Day 10", () => {
   it(`part 1 solves for the example input`, () => {
     expect(day10.solveForPartOne(example)).toBe("26397");
   });
+
+  it(`Should calculate the right autocomplete score`, () => {
+    expect(day10.getAutoCompleteScore('[({(<(())[]>[[{[]{<()<>>'.split(''))).toBe(288957);
+  });
+  it(`part 2 solves for the example input`, () => {
+    expect(day10.solveForPartTwo(example)).toBe("288957");
+  });
 });
--- src/day10/index.ts
+++ src/day10/index.ts
@@ -11,12 +11,25 @@ class Day10 extends Day {
     "}": 1197,
     ">": 25137,
   };
+  completeTable: { [key: string]: number } = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+  };
   openers = "([{<";
   openerFor: { [key: string]: string } = {
     ")": "(",
     "]": "[",
     "}": "{",
     ">": "<",
+  };
+
+  closerFor: { [key: string]: string } = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
   };
 
   score(line: string[]): number {
@@ -50,8 +63,38 @@ class Day10 extends Day {
       .toString();
   }
 
+  getAutoCompleteScore(line: string[]): number {
+    let stack: string[] = [];
+
+    for (const char of line) {
+      if (this.openers.indexOf(char) >= 0) {
+        stack.push(char);
+      } else {
+        stack.pop();
+      }
+    }
+
+    return stack
+      .reverse()
+      .map((x) => this.completeTable[this.closerFor[x]])
+      .reduce((acc, cur) => {
+        acc = acc * 5 + cur;
+        return acc;
+      }, 0);
+  }
+
   solveForPartTwo(input: string): string {
-    return input;
+    let lines = input
+      .split(/[\r\n]/g)
+      .filter(Boolean)
+      .map((l) => l.split(""))
+      .filter((l) => this.score(l) == 0);
+
+    let scores = lines
+      .map((l) => this.getAutoCompleteScore(l))
+      .sort((x, y) => x - y);
+
+    return scores[Math.floor(scores.length / 2)].toString();
   }
 }
 
