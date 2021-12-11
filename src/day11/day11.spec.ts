af74284b664d1f4738265e2249743c479fe1c703
Loris Bognanni <loris@codemade.co.uk>
2021-12-11 18:22:35 +0000

    :gift: Day 11 part 1

---
+++ src/day11/day11.spec.ts
@@ -0,0 +1,18 @@
+import day11 from "./index";
+
+const example = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`;
+
+describe("On Day 11", () => {
+  it(`part 1 solves for the example input`, () => {
+    expect(day11.solveForPartOne(example)).toBe("1656");
+  });
+});
---
+++ src/day11/index.ts
@@ -0,0 +1,70 @@
+import { Day } from "../day";
+
+class Day11 extends Day {
+  constructor() {
+    super(11);
+  }
+
+  getMap(input: string): number[][] {
+    return input
+      .split(/[\r\n]/g)
+      .filter(Boolean)
+      .map((line) => line.split("").map((x) => parseInt(x, 10)));
+  }
+
+  runMap(map: number[][]): number {
+    let flashes = 0;
+
+    function flash(x: number, y: number) {
+      if (x < 0 || y < 0 || x == map[0].length || y == map.length) return;
+      if (map[y][x] > 9) return;
+      map[y][x]++;
+      if (map[y][x] > 9) {
+        flashes++;
+
+        flash(x - 1, y - 1);
+        flash(x + 1, y - 1);
+        flash(x, y - 1);
+
+        flash(x - 1, y);
+        flash(x + 1, y);
+
+        flash(x - 1, y + 1);
+        flash(x + 1, y + 1);
+        flash(x, y + 1);
+      }
+    }
+
+    function forEveryCell(fun: (x: number, y: number) => void) {
+      for (let y = 0; y < map.length; ++y) {
+        for (let x = 0; x < map[y].length; ++x) {
+          fun(x, y);
+        }
+      }
+    }
+
+    forEveryCell((x, y) => flash(x, y));
+    forEveryCell((x, y) => {
+      if (map[y][x] > 9) {
+        map[y][x] = 0;
+      }
+    });
+
+    return flashes;
+  }
+
+  solveForPartOne(input: string): string {
+    const map = this.getMap(input);
+    let flashes = 0;
+    for (let i = 0; i < 100; ++i) {
+      flashes += this.runMap(map);
+    }
+    return flashes.toString();
+  }
+
+  solveForPartTwo(input: string): string {
+    return input;
+  }
+}
+
+export default new Day11();
