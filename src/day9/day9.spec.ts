78a40a65d1875525d3f5e02d59168967751aa0bd
Loris Bognanni <loris@codemade.co.uk>
2021-12-11 17:28:20 +0000

    :gift: day 9 part 2

--- src/day9/day9.spec.ts
+++ src/day9/day9.spec.ts
@@ -9,5 +9,14 @@ const example = `2199943210
 describe('On Day 9', () =>{
     it(`part 1 solves for the example input`, ()=>{
         expect(day9.solveForPartOne(example)).toBe('15');
-    })
+    });
+
+    it(`calculates the right basin size`, () => {
+        expect(day9.getBasinSize(day9.getMap(example), {x: 0, y: 0})).toBe(3);
+        expect(day9.getBasinSize(day9.getMap(example), {x: 9, y: 0})).toBe(9);
+    });
+
+    it(`part 2 solves for the example input`, ()=>{
+        expect(day9.solveForPartTwo(example)).toBe('1134');
+    });
 });
--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,16 +1,24 @@
 import { Day } from "../day";
 
+type Point = {
+  x: number;
+  y: number;
+};
+
 class Day9 extends Day {
   constructor() {
     super(9);
   }
 
-  solveForPartOne(input: string): string {
-    const map = input
+  getMap(input: string): number[][] {
+    return input
       .split(/[\r\n]/g)
       .filter(Boolean)
       .map((line) => line.split("").map((x) => parseInt(x, 10)));
-    let lowSum = 0;
+  }
+
+  getLowPoints(map: number[][]): Point[] {
+    let arr: Point[] = [];
 
     for (let x = 0; x < map[0].length; ++x) {
       for (let y = 0; y < map.length; ++y) {
@@ -29,16 +37,58 @@ class Day9 extends Day {
         }
 
         if (map[y][x] < nearby.reduce((prev, cur) => Math.min(prev, cur), 10)) {
-          lowSum += map[y][x] + 1;
+          arr.push({ x, y });
         }
       }
     }
 
-    return lowSum.toString();
+    return arr;
+  }
+
+  solveForPartOne(input: string): string {
+    const map = this.getMap(input);
+
+    return this.getLowPoints(map)
+      .reduce((acc, p) => {
+        acc += map[p.y][p.x] + 1;
+        return acc;
+      }, 0)
+      .toString();
+  }
+
+  getBasinSize(map: number[][], pt: Point): number {
+    let points: Point[] = [];
+
+    function getBasin(pt: Point) {
+      if (pt.x < 0 || pt.y < 0 || pt.x == map[0].length || pt.y == map.length)
+        return;
+      if (map[pt.y][pt.x] == 9) return;
+      if (points.filter((p) => p.x == pt.x && p.y == pt.y).length > 0) return;
+      points.push(pt);
+      getBasin({ x: pt.x - 1, y: pt.y });
+      getBasin({ x: pt.x + 1, y: pt.y });
+      getBasin({ x: pt.x, y: pt.y - 1 });
+      getBasin({ x: pt.x, y: pt.y + 1 });
+    }
+
+    getBasin(pt);
+
+    return points.length;
   }
 
   solveForPartTwo(input: string): string {
-    return "input";
+    let map = this.getMap(input);
+    let lowPoints = this.getLowPoints(map);
+
+    return lowPoints
+      .map((pt) => this.getBasinSize(map, pt))
+      .sort((a, b) => b - a)
+      .slice(0, 3)
+      .reduce((acc, cur) => {
+        acc *= cur;
+        return acc;
+      }, 1)
+      .toString();
   }
 }
 
