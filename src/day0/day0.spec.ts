ef0322193cbe6f401ec97a80b59b2b8b81497b5c
Loris Bognanni <loris@codemade.co.uk>
2021-12-21 16:28:20 +0000

    :art: auto-format day0

--- src/day0/day0.spec.ts
+++ src/day0/day0.spec.ts
@@ -1,9 +1,9 @@
-import day0 from './index';
+import day0 from "./index";
 
 const example = `hello`;
 
-describe('On Day 0', () =>{
-    it(`part 1 solves for the example input`, ()=>{
-        expect(day0.solveForPartOne(example)).toBe('hello');
-    })
+describe("On Day 0", () => {
+  it(`part 1 solves for the example input`, () => {
+    expect(day0.solveForPartOne(example)).toBe("hello");
+  });
 });
